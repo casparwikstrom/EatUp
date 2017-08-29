523d725db810c2272c8d8e43c128dab530bb59ce
Leon Entrup <luenneberger@gmail.com>
2017-08-29 11:06:10 +0200

    Added new header font

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170828150025) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,8 +29,8 @@ ActiveRecord::Schema.define(version: 20170828150025) do
     t.datetime "updated_at"
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
   end
-  
- create_table "comments", force: :cascade do |t|
+
+  create_table "comments", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
     t.bigint "popup_id"
@@ -39,7 +38,6 @@ ActiveRecord::Schema.define(version: 20170828150025) do
     t.datetime "updated_at", null: false
     t.index ["popup_id"], name: "index_comments_on_popup_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-
   end
 
   create_table "orders", force: :cascade do |t|
@@ -106,10 +104,8 @@ ActiveRecord::Schema.define(version: 20170828150025) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "comments", "popups"
   add_foreign_key "comments", "users"
-
   add_foreign_key "orders", "pledges"
   add_foreign_key "orders", "popups"
   add_foreign_key "orders", "users"
