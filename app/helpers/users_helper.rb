86a6caf11bae1740e7053f807ad7bae4caa801e9
Jasmine Chao <ejasminechao@gmail.com>
2017-09-05 11:49:32 +0200

    edits made

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,9 @@
 module UsersHelper
   def user_avatar(user)
-    facebook_url = current_user.facebook_picture_url.split("?")[0] + "?type=large"
     if user.photo.present?
       cl_image_path(user.photo.path)
-    elsif facebook_url
-      facebook_url
+    elsif current_user.facebook_picture_url
+      current_user.facebook_picture_url.split("?")[0] + "?type=large"
       # Google has the same path
     else
       "http://res.cloudinary.com/eatupclub/image/upload/v1504254820/Logo_v1_ioepmg.png"
