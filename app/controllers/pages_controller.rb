3ac0302daf3814038e7b1b793ff96d980335fc29
Caspar Wikstrom <caspar.wikstrom@gmail.com>
2017-09-05 15:19:02 +0200

    maps almost done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,29 +6,33 @@ class PagesController < ApplicationController
     @wishlist = Wishlist.new
   end
 
+
   def index
     @wishlist = Wishlist.new
+
     if params[:type_ids]
-      @popups = policy_scope(Popup).joins(:types).where(types: { id: params[:type_ids]}).select(&:is_ready?)
+      @popups = policy_scope(Popup).joins(:types).where.not(latitude: nil, longitude: nil).where(types: { id: params[:type_ids]}).select(&:is_ready?)
+
       @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
         marker.lat popup.latitude
         marker.lng popup.longitude
       end
 
-    elsif params[:query]
-      terms = params[:query].split
-      query = terms.map { |term| "LOWER(title) like '%#{term.downcase}%' OR LOWER(description) like '%#{term.downcase}%' OR LOWER(address) like '%#{term.downcase}%' OR LOWER(types.name) like '%#{term.downcase}%'" }.join(" OR ")
-      @popups = policy_scope(Popup).joins(:types).where(query).order(created_at: :desc).uniq.select(&:is_ready?)
+    elsif params[:search]
+      terms = params[:search].split
+      search = terms.map { |term| "LOWER(title) like '%#{term.downcase}%' OR LOWER(description) like '%#{term.downcase}%' OR LOWER(address) like '%#{term.downcase}%' OR LOWER(types.name) like '%#{term.downcase}%'" }.join(" OR ")
+      @popups = policy_scope(Popup).joins(:types).where.not(latitude: nil, longitude: nil).where(search).order(created_at: :desc).uniq.select(&:is_ready?)
+
       @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
         marker.lat popup.latitude
         marker.lng popup.longitude
       end
-      # query = params[:query].presence || '*'
-      # @search = Popup.search(query)
+      # search = params[:search].presence || '*'
+      # @search = Popup.search(search)
       # @popups = @search.results
     else
+      @popups = policy_scope(Popup).where.not(latitude: nil, longitude: nil).order(created_at: :desc).select(&:is_ready?)
 
-      @popups = policy_scope(Popup).order(created_at: :desc).select(&:is_ready?)
       @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
         marker.lat popup.latitude
         marker.lng popup.longitude
