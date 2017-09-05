dad8d10a0667139916ded06c143d18fbb6f0790c
Caspar Wikstrom <caspar.wikstrom@gmail.com>
2017-09-05 14:29:51 +0200

    trying to solve the map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,16 +7,38 @@ class PagesController < ApplicationController
   end
 
   def index
-    @category = params[:category]
-    @place = params[:address]
-    @popups = Popup.joins(:types).where.not(latitude: nil, longitude: nil).near(@place, 10).where(types: {name: @category.capitalize }).distinct unless @place.blank? || @category.blank?
-    @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
-      marker.lat popup.latitude
-      marker.lng popup.longitude
-    end
-    #@popups = @popups.near(params[:address], 10) unless params[:address].blank?
-    # Popup.joins(:types).where.not(latitude: nil, longitude: nil).near(params[:address], 10).where(types: {name: @category })
+    @wishlist = Wishlist.new
+    if params[:type_ids]
+      @popups = policy_scope(Popup).joins(:types).where(types: { id: params[:type_ids]}).select(&:is_ready?)
+      @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
+        marker.lat popup.latitude
+        marker.lng popup.longitude
+      end
 
+    elsif params[:query]
+      terms = params[:query].split
+      query = terms.map { |term| "LOWER(title) like '%#{term.downcase}%' OR LOWER(description) like '%#{term.downcase}%' OR LOWER(address) like '%#{term.downcase}%' OR LOWER(types.name) like '%#{term.downcase}%'" }.join(" OR ")
+      @popups = policy_scope(Popup).joins(:types).where(query).order(created_at: :desc).uniq.select(&:is_ready?)
+      @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
+        marker.lat popup.latitude
+        marker.lng popup.longitude
+      end
+      # query = params[:query].presence || '*'
+      # @search = Popup.search(query)
+      # @popups = @search.results
+    else
+
+      @popups = policy_scope(Popup).order(created_at: :desc).select(&:is_ready?)
+      @hash = Gmaps4rails.build_markers(@popups) do |popup, marker|
+        marker.lat popup.latitude
+        marker.lng popup.longitude
+      end
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
       # marker.infowindow render_to_string(partial: "/popups/map_box", locals: { popup: popup })
