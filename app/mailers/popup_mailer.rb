0d37ce36e723061872971969431a20ab4e69d33e
Caspar Wikstrom <caspar.wikstrom@gmail.com>
2017-09-06 16:18:18 +0200

    adding stuff to emails

--- app/mailers/popup_mailer.rb
+++ app/mailers/popup_mailer.rb
@@ -8,4 +8,14 @@ class PopupMailer < ApplicationMailer
       subject:  "popup #{@order.popup.title} created!"
     )
   end
+
+  def userorder_confirmation(popup)
+
+    @popup = popup
+
+    mail(
+      to:       @popup.user.email,
+      subject:  "popup #{@order.popup.title} created!"
+    )
+  end
 end
